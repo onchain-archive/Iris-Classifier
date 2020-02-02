a2b3f5c6bc1197dd54aaff4684d4dcaae3b25cac
oscillator25 <pratyaksh1615@gmail.com>
2020-02-02 15:45:22 +0530

    used KNeighborsClassifier

---
+++ code.py
@@ -0,0 +1,22 @@
+#load dataset
+from sklearn import datasets
+iris = datasets.load_iris()
+x = iris.data
+y = iris.target
+
+#splitting the dataset
+from sklearn.model_selection import train_test_split
+x_train, x_test,y_train,y_test=train_test_split(x,y,test_size=0.5)
+
+#bulding the model
+from sklearn import neighbors
+classifier = neighbors.KNeighborsClassifier()
+
+#training the model
+classifier.fit(x_train,y_train)
+
+#make predictions
+predictions = classifier.predict(x_test)
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test, predictions))
